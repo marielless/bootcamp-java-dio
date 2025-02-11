7dbadd570ffddfe236549ad4e891b94176f8ee32
marielless <mariellesantos96@hotmail.com>
2025-02-11 11:38:27 -0300

    Adiciona condição ternária na classe ResultadoEsperado.

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,9 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+
+        String resultado = mediaFinal >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+    }
+}
