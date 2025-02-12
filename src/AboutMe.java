44991bde144f27149f7f9089e473bcade8b1b661
marielless <mariellesantos96@hotmail.com>
2025-02-12 10:16:17 -0300

    Implementa tratamento de exceções com try/catch na classe AboutMe.

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,24 +1,33 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome:");
-        String nome = scanner.next();
+        try {
 
-        System.out.println("Digite seu sobrenome:");
-        String sobrenome = scanner.next();
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite sua idade:");
-        int idade = scanner.nextInt();
+            System.out.println("Digite seu nome:");
+            String nome = scanner.next();
 
-        System.out.println("Digite sua altura:");
-        double altura = scanner.nextDouble();
+            System.out.println("Digite seu sobrenome:");
+            String sobrenome = scanner.next();
 
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm ");
+            System.out.println("Digite sua idade:");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura:");
+            double altura = scanner.nextDouble();
+
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + " cm ");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos idade e altura devem ser númericos");
+        }
+
     }
 }
